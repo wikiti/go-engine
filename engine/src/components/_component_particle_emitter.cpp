884b9934e3fdd89b1cc44b79e44d18d21b5f7540
Wikiti <wikiti.doghound@gmail.com>
2013-11-25 19:17:33 +0000

    Añadida función para mover la cámara con el ratón - arreglados commits.

--- engine/src/components/_component_particle_emitter.cpp
+++ engine/src/components/_component_particle_emitter.cpp
@@ -163,15 +163,12 @@ void CComponent_Particle_Emitter::OnRender()
 
     glTranslatef((*it)->position.x, (*it)->position.y, (*it)->position.z);
 
-    // Esta parte es MUY problemática, ya que jode por completo el efecto de profundidad, y crea efectos muy extraños.
     double MV[16];
     glGetDoublev(GL_MODELVIEW_MATRIX, MV);
     makebillboard_mat4x4(MV, MV);
     glLoadMatrixd(MV);
-    // Fin parte problemática
 
     glRotatef((*it)->angle, 0.f, 0.f, 1.f);
-
     glScalef((*it)->scale.x, (*it)->scale.y, 1.f);
 
     glBegin(GL_TRIANGLE_STRIP);
