d065180a09d30d29f260418fa4c2c94f42b45fef
Wikiti <wikiti.doghound@gmail.com>
2014-01-25 16:57:15 +0000

    Se han creado varias funciones para el sistema CSystem_UserInput
    - Básicamente, se han creado funciones (y atributos) para controlar la entrada del usuario.
      - De una manera sencilla, se pueden gestionar los eventos de teclado (de momento).

--- engine/include/systems/_input.h
+++ engine/include/systems/_input.h
@@ -6,15 +6,79 @@
 
 class CSystem_UserInput: public CSystem
 {
-  protected:
-    friend class CSystem_Debug;
+  friend class CSystem_Debug;
 
+  protected:
+    class CKey
+    {
+      friend class CSystem_UserInput;
+
+      protected:
+        bool state;
+        SDL_Scancode key;
+
+      public:
+        bool operator()() {return state;}
+        bool State(){return state;}
+        SDL_Scancode Key() {return key;}
+        char* KeyName() {return SDL_GetScancodeName(key);}
+    };
+
+    class CAxis
+    {
+      friend class CSystem_UserInput;
+
+      public:
+       float vertical;
+       float horizontal;
+
+       CKey v_up, v_down, h_left, h_right;
+    };
+
+    // raw keyboard
+    const Uint8 *keyboard;
+
+  public:
+      // Axis - para el futuro, se sopotarán joysticks
+    CAxis axis1; // wasd
+    CAxis axis2; // arrows
+
+      // Keys/actions
+    CKey fire1;   // mouse1
+    CKey fire2;   // mouse2
+    CKey action1; // n
+    CKey action2; // m
+    CKey action3; // j
+
+    CKey run;     // L-shift
+    CKey crouch;  // L-cntrl
+    CKey jump;    // space
 
   public:
     CSystem_UserInput(): CSystem() {};
 
     bool Init();
     void Close();
+
+    void OnEvent();
+
+    Uint8 Keyboard(string keyname);
+    Uint8 Keyboard(SDL_Scancode key);
+
+    CAxis GetAxis1(){return axis1();}
+    CAxis GetAxis2(){return axis2();}
+
+      // Keys/actions
+    bool GetFire1(){return fire1();}
+    bool GetFire2(){return fire2();}
+    bool GetAction1(){return action1();}
+    bool GetAction2(){return action2();}
+    bool GetAction3(){return action3();}
+
+    bool GetRun(){return run();}
+    bool GetCrouch(){return crouch();}
+    bool GetJump(){return jump();}
+
 };
 
 // Crear 2 objetos: 1 para la configuración de la aplicación, y otra para guardar variables de usuario.
--- engine/src/systems/_input.cpp
+++ engine/src/systems/_input.cpp
@@ -3,3 +3,42 @@
 CSystem_UserInput gSystem_UserInput;
 CSystem_UserInput& gUserInput = gSystem_UserInput;
 
+const Uint8 *keyboard = SDL_GetKeyboardState(NULL);
+
+bool CSystem_UserInput::Init()
+{
+    // Axis
+  axis1.horizontal = axis2.horizontal = 0;
+  axis1.vertical = axis2.vertical = 0;
+
+
+    // Actions
+  fire1.state = fire2.state = false;
+  action1.state = action2.state = action3.state = false;
+  run.state = crouch.state = jump.state = false;
+
+  return true;
+}
+
+void CSystem_UserInput::Close()
+{
+
+}
+
+void CSystem_UserInput::OnEvent()
+{
+
+}
+
+Uint8 CSystem_UserInput::Keyboard(string keyname)
+{
+  SDL_Scancode key = SDL_GetScancodeFromName(keyname.c_str());
+  return Keyboard(key);
+}
+
+Uint8 CSystem_UserInput::Keyboard(SDL_Scancode key)
+{
+  if(key == SDL_SCANCODE_UNKNOWN)
+    return -1;
+  return keyboard[key];
+}
