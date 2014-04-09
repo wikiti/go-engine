6ac16b574d174a073ef608c8c75e9ad80e564995
Wikiti <wikiti.doghound@gmail.com>
2014-04-09 10:27:06 +0000

    Definido (en parte) documentación para la clase CInstance.

--- engine/include/engine/_instance.h
+++ engine/include/engine/_instance.h
@@ -1,8 +1,37 @@
+/**
+ * @file
+ * @brief Fichero que incluye la clase CInstance.
+ */
+
 #ifndef __INSTANCE_H_
 #define __INSTANCE_H_
 
 #include "_globals.h"
 
+/** @addtogroup Otros */
+/*@{*/
+
+/**
+ * @brief Clase que representa a una estancia o nivel.
+ *
+ * Una estancia o nivel representa un escenario concreto de nuestra aplicación o nivel.
+ * Por ejemplo, supóngase el caso en el que nuestro juego esta compuesto de varios subjuegos (3 en raya,
+ * ajedrez, póker...). Cada juego podría corresponderse con una estancia, y el menú de selección de juego
+ * también podría considerarse una estancia o nivel.
+ *
+ * Así pues, una estancia de componene de:
+ * - Una serie de objetos de juego, cargados desde el callback de carga de objetos (véase CInstance() ).
+ * - Una serie de recursos, sean texturas, modelos, sonidos... cargados directamente desde un fichero de recursos *.rc*.
+ * - Una próxima estancia a ejecutar (si no hay próxima, finaliza el juego).
+ *
+ * Sin más, el motor gráfica u objeto gEngine se encargará de gestionar las estancias, de manera que el usuario sólo
+ * tiene que definirlas, definir la próxima estancia de cada una (se puede hacer de manera dinámica) y estblecer un
+ * fichero de recursos a cargar.
+ *
+ * @see CEngine
+ * @see gEngine
+ *
+ */
 class CInstance
 {
   protected:
@@ -21,10 +50,6 @@ class CInstance
 
     bool first_frame;
 
-  public:
-    CInstance(fboolpointer load_gameObject_function, std::string resource_file);
-    //~CInstance();
-
     bool Init();
       bool LoadResources();
 
@@ -37,6 +62,20 @@ class CInstance
     void OnRender();
     void CalculateFPS();
 
+  public:
+    /**
+     * @brief Constructor principal.
+     *
+     * Crea una estancia. No debe llamarse a este constructor de manera dinámica, sino que
+     * debe ser creada por el método CEngine::AddInstance(). Se caracteriza porque se le proporciona
+     * un callback de los objetos a crear, y un fichero de recursos.
+     *
+     * @param load_gameObject_function
+     * @param resource_file
+     */
+    CInstance(fboolpointer load_gameObject_function, std::string resource_file);
+    //~CInstance();
+
     GLdouble fps()
     {
       return FPS;
@@ -48,5 +87,7 @@ class CInstance
 
     //virtual void OnDebug(){};
 };
+
+/*@}*/
 
 #endif /* __INSTANCE_H_ */
