3046c618c9d37ba0f018de9e8ae8e76d5edbcc72
Wikiti <wikiti.doghound@gmail.com>
2014-04-16 17:14:28 +0000

    Añadido shader de ilumincación "phong".

--- data/shaders/phong_frag.glsl
+++ data/shaders/phong_frag.glsl
@@ -3,29 +3,22 @@
 uniform sampler2D texture;
 uniform float textureFlag;
 
-varying vec3 frag_normal;
-varying vec3 frag_vertex_to_light_vector;
+varying vec3 diffuseColor;
+varying vec3 specularColor;
 
 varying vec4 frag_Color;
-varying vec2 frag_TexCoords;
+varying vec3 frag_TexCoords;
 
 void main(void)
 {
-    // Defining The Material Colors
-    const vec4 AmbientColor = vec4(0.1, 0.0, 0.0, 1.0);
-    const vec4 DiffuseColor = vec4(1.0, 0.0, 0.0, 1.0);
+  //gl_FragColor = texture2D(texture, texCoords);
+  
+  vec2 longitudeLatitude = vec2(
+    (atan(frag_TexCoords.y, frag_TexCoords.x) / 3.1415926 + 1.0) * 0.5, 
+     1.0 - acos(frag_TexCoords.z) / 3.1415926);
+     // unusual processing of texture coordinates
  
-    // Scaling The Input Vector To Length 1
-    vec3 normalized_normal = normalize(frag_normal);
-    vec3 normalized_vertex_to_light_vector = normalize(frag_vertex_to_light_vector);
- 
-    // Calculating The Diffuse Term And Clamping It To [0;1]
-    float DiffuseTerm = clamp(dot(frag_normal, frag_vertex_to_light_vector), 0.0, 1.0);
- 
-    // Calculating The Final Color
-    gl_FragColor = AmbientColor + DiffuseColor * DiffuseTerm;
-
-    //gl_FragColor = frag_Color;
-    //gl_FragColor = texture2D(texture, frag_TexCoords).rgba * frag_Color;
-    //mix(frag_Color, texture2D(texture, frag_TexCoords) * frag_Color, textureFlag);
+  gl_FragColor = vec4(diffuseColor 
+    * vec3(texture2D(texture, longitudeLatitude))
+    + specularColor, 1.0);
 }
--- data/shaders/phong_vert.glsl
+++ data/shaders/phong_vert.glsl
@@ -2,31 +2,81 @@
 
 uniform mat4 ProjMatrix;
 uniform mat4 ModelViewMatrix;
+uniform mat4 NormalMatrix;
 
 attribute vec4 in_Position;
+attribute vec4 in_Normal;
 attribute vec4 in_Color;
-attribute vec2 in_TexCoords;
+attribute vec3 in_TexCoords;
 
 varying vec4 frag_Color;
-varying vec2 frag_TexCoords;
+varying vec3 frag_TexCoords;
 
-varying vec3 frag_normal;
-varying vec3 frag_vertex_to_light_vector;
-
-void main(void)
+varying vec3 frag_diffuseColor; 
+            // the diffuse Phong lighting computed in the vertex shader
+varying vec3 frag_specularColor; 
+            // the specular Phong lighting computed in the vertex shader
+ 
+void main()
 {
-    gl_Position = ProjMatrix * ModelViewMatrix * in_Position;
- 
-    // Transforming The Normal To ModelView-Space
-    frag_normal = gl_NormalMatrix * gl_Normal;
- 
-    // Transforming The Vertex Position To ModelView-Space
-    vec4 vertex_in_modelview_space = ModelViewMatrix * in_Position;
- 
-    // Calculating The Vector From The Vertex Position To The Light Position
-    frag_vertex_to_light_vector = vec3(vec3(0, 0, 0) * vertex_in_modelview_space);
-    
-    frag_Color = in_Color;
-    frag_TexCoords = in_TexCoords;
-}
+  //mat4 NormalMatrix = transpose(inverse(modelView));
+  mat4 MVPMatrix = ProjMatrix * ModelViewMatrix;
 
+  vec3 normalDirection =  normalize(NormalMatrix * in_Normal);
+  vec3 viewDirection = -normalize(vec3(ModelViewMatrix * in_Position)); 
+  vec3 lightDirection;
+  float attenuation;
+ 
+  if (0.0 == gl_LightSource[0].position.w) 
+  // directional light?
+  {
+    attenuation = 1.0; // no attenuation
+    lightDirection = 
+    normalize(vec3(gl_LightSource[0].position));
+  } 
+  else // point light or spotlight (or other kind of light) 
+  {
+    vec3 vertexToLightSource = vec3(gl_LightSource[0].position - ModelViewMatrix * in_Position);
+    float distance = length(vertexToLightSource);
+    attenuation = 1.0 / distance; // linear attenuation 
+    lightDirection = normalize(vertexToLightSource);
+ 
+    if (gl_LightSource[0].spotCutoff <= 90.0) // spotlight?
+    {
+      float clampedCosine = max(0.0, dot(-lightDirection, 
+      gl_LightSource[0].spotDirection));
+      if (clampedCosine < gl_LightSource[0].spotCosCutoff) 
+      // outside of spotlight cone?
+      {
+        attenuation = 0.0;
+      }
+      else
+      {
+        attenuation = attenuation * pow(clampedCosine, 
+        gl_LightSource[0].spotExponent);
+      }
+    }
+  }
+ 
+  vec3 ambientLighting = vec3(gl_LightModel.ambient); 
+  // without material color!
+  vec3 diffuseReflection = attenuation * vec3(gl_LightSource[0].diffuse) * max(0.0, dot(normalDirection, lightDirection)); 
+  // without material color!
+ 
+  vec3 specularReflection;
+  if (dot(normalDirection, lightDirection) < 0.0) 
+  // light source on the wrong side?
+  {
+    specularReflection = vec3(0.0, 0.0, 0.0); 
+    // no specular reflection
+  }
+  else // light source on the right side
+  {
+    specularReflection = attenuation * vec3(gl_LightSource[0].specular)  * vec3(gl_FrontMaterial.specular)  * pow(max(0.0, dot(reflect(-lightDirection, normalDirection), viewDirection)), gl_FrontMaterial.shininess);
+  }
+ 
+  frag_diffuseColor = ambientLighting + diffuseReflection;
+  frag_specularColor = specularReflection;
+  frag_TexCoords = in_TexCoords; // Cambio
+  gl_Position = MVPMatrix * in_Position;
+}
